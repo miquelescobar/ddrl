209abbd65fa4f9348b40298790989a9b71c3d3c3
Miquel Escobar <miqui99@gmail.com>
2021-06-24 21:39:47 +0200

    CTE-POWER updates

---
+++ cte-power/jobs/distributed/train-humanoid-ppo-distributed.sh
@@ -0,0 +1,3 @@
+python start-distributed.py --exp-name "train-humanoid-ppo-dist-time" --num-nodes 2 --num-gpus 1 \
+                            --load-env "module load gcc/8.3.0 cuda/10.2 cudnn/7.6.4 nccl/2.4.8 tensorrt/6.0.1 openmpi/4.0.1 atlas/3.10.3 scalapack/2.0.2 fftw/3.3.8 szip/2.1.1 ffmpeg/4.2.1 opencv/4.1.1 python/3.7.4_ML ray/1.1.0;"
+                            --command "python train-bullet-envs.py --params-file ../../trainings/pybullet-envs/humanoid-ppo-dist-time-gpu.json"
